a7e6bc4b0c86a5715044d2c85fece962a2593058
amberlu <xiaoyinglu59@gmail.com>
2024-03-05 19:44:24 -0400

    keep cart session when logout

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -88,7 +88,14 @@ def my_login(request):
 
 
 def user_logout(request):
-    auth.logout(request)
+    try:
+        for key in list(request.session.keys()):
+            if key == 'session_key':
+                continue
+            else:
+                del request.session[key]
+    except KeyError:
+        pass
     return redirect("store")
 
 
